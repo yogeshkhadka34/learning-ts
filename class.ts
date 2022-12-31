a4c8be179318ed569b1bf8f966f5eb8f52981e55
Yogesh <yogeshkhadka34@gmail.com>
2022-12-31 19:30:06 +0545

    for in and for of

---
+++ class.ts
@@ -0,0 +1,20 @@
+
+
+//for in
+let colors = ["red","green","blue"]
+
+for(let color in colors){
+    console.log(colors[color]); 
+}
+//for of
+for(let index of colors){
+    console.log(index);
+    
+}
+//splitting letters of string by for of
+let letters = "ABC";
+
+for (let letter of letters){
+    console.log(letter);
+    
+}
