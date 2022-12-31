3c35ae8069b280809439bb14a703882d60598b92
Yogesh <yogeshkhadka34@gmail.com>
2022-12-31 19:13:40 +0545

    String template, destructuring of object and array

--- object.js
+++ object.js
@@ -2,18 +2,19 @@
 let firstname = "Chanler";
 let lastname = "Bing";
 let fullname;
-//console.log(firstname+" "+lastname);
+console.log(firstname + " " + lastname);
 let person = {
     firstname,
     lastname,
     fullname
 };
 let createPerson = function (firstname, lastname, fullname) {
-    person.firstname = firstname;
-    return person;
+    //fullname = firstname+" "+lastname;
+    return { firstname, lastname, fullname };
 };
-let p = createPerson("Rei", "Reid", "Rai");
+let p = createPerson("Reiley", "Reid", "Rai");
 console.log(p.firstname);
-// console.log(p.lastname);
-// console.log(p.fullname);
+console.log(p.lastname);
+console.log(p.fullname);
 console.log("hi");
+
--- object.ts
+++ object.ts
@@ -4,6 +4,12 @@ let lastname= "Bing";
 let fullname;
 //console.log(firstname+" "+lastname);
 
+//String templates
+console.log(`Welcome to internet ${firstname}
+Say hello please
+`);
+
+
 let person= {
     firstname,
     lastname,
@@ -18,5 +24,20 @@ let p = createPerson("Rei", "Reid","Rai");
 console.log(p.firstname);
 // console.log(p.lastname);
 // console.log(p.fullname);
-console.log("hi");
+
+//Destructuring of Object
+let {firstname:f, lastname:l, fullname:fl}= person;
+console.log(person);
+console.log(f);
+
+
+//Destructuring of array
+fullname = [firstname,lastname];
+console.log(fullname);
+//let [first,last,third] = fullname;
+let [,,third="thirds"]=fullname;
+//console.log(first);
+//console.log(last);
+console.log(third);
+
 
